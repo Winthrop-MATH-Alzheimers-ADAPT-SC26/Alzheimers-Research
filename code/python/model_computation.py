bb046d0dda8649af36b4c2acdf630497c33e826a
gnix22 <gjnix22@gmail.com>
2026-06-02 15:53:03 -0400

    update to python class

--- code/python/model_computation.py
+++ code/python/model_computation.py
@@ -3,11 +3,11 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 # define the class for system of equations
 class ODEModel:
-    def __init__(self, params): # params list is ordered: 
-                                # a1, a2, b1, b2, c1, c2, c3,
-                                # d1, d2, e1, e2, e3, u1, u2,
-                                # e3, u1, u2, u3, k1, k2, k3,
-                                # k4, k5, sigma, r.
+    def __init__(self, params, initial_conditions, t_span, t_eval): 
+        # params list is ordered: # a1, a2, b1, b2, c1, c2, c3,
+        # d1, d2, e1, e2, e3, u1, u2,
+        # e3, u1, u2, u3, k1, k2, k3,
+        # k4, k5, sigma, r.
         self.a1 = params[0]
         self.a2 = params[1]
         self.b1 = params[2]
@@ -30,41 +30,40 @@ class ODEModel:
         self.k5 = params[19]
         self.sigma = params[20]
         self.r = params[21]
-        print("...parameters loaded...")
+
+        self.init_cond = initial_conditions
+        self.t_span = t_span
+        self.t_eval = t_eval
     # use call to allow for solutions to be computed later
     def __call__(self, t, y):
-        ABeta, Ca, Tau, N, C = y # 
+        ABeta, Ca, Tau, N, C = y
         dABeta = self.a1 + self.a2 * (Ca**2 / (Ca**2 + self.sigma**2)) - self.k1 * ABeta - self.u1 * ABeta
         dCa = self.b1 + self.b2 * ABeta - self.k2 * Ca - self.u2 * Ca
         dTau = self.c1 + self.c2 * ABeta + self.c3 * Ca - self.k3 * Tau - self.u3 * Tau
         dN = self.d1 + self.d2 * Tau - self.k4 * N
         dC = self.e1 + self.e2 * N * self.r + self.e3 * Tau - self.k5 * C
-        return [dABeta, dCa, dTau, dN, dC] #
+        return [dABeta, dCa, dTau, dN, dC]
+    def solution(self):
+        solution = solve_ivp(self, self.t_span, self.init_cond, t_eval=self.t_eval, method='LSODA')
+        return solution
+    def results(self):
+        print("...calculated iterations...")
+        print(*self.solution().y, sep=", ")
 
 # given arbitrary parameters
 params = [65641, 15778.463, 315569260, 6311385.2, 52.2958, 1.78467,
           3.62493, 0.07176, 0.0195, 146.308032, 86.84, 199.16, 0.5, 0.5, 0.5,
           3035.98, 21874000, 10.9999, 0.003588, 0.8684, 0.00027, 1]
-model_no_treatment = ODEModel(params)
-
 initial_conditions = [0.0, 0.0, 0.0, 0.0, 0.0] # initial conditions [ABeta_0, Ca_0], 0.0, 0.0, 0.0
 t_span = (0, 100)
 t_eval = np.linspace(*t_span, 100)
-
+model_no_treatment = ODEModel(params, initial_conditions, t_span, t_eval)
+# start solver for ODEs
 print("...starting solver...")
-sol = solve_ivp(model_no_treatment, t_span, initial_conditions, t_eval=t_eval, method='LSODA')
-print("...calculated solutions...")
-#sol.y[0] -> ABeta over time
-#sol.y[1] -> Ca over time
-print(sol.y[0])
-print(sol.y[1])
-print(sol.y[2])
-print(sol.y[3])
-print(sol.y[4])
+model_no_treatment.results()
 
 # create a grid with 2 rows and 2 columns
 #fig, axs = plt.subplots(2, 2)
-
 # access a specific subplot using 2D indexing [row, col]
 #axs[0, 0].plot(t_eval, sol.y[0])
 #axs[0, 1].plot(t_eval, sol.y[1])
