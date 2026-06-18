464e6d83353c156897c1440c68b5bde00a7ef3d7
Steven <stevenstokes1020@gmail.com>
2026-06-18 14:19:50 -0400

    put new OC img in manuscript

--- code/julia/OC-IndividualTreatment.jl
+++ code/julia/OC-IndividualTreatment.jl
@@ -14,17 +14,17 @@ weights3 = calculate_weights(sys, pvec, tspan, (0.0, 0.0, umax[3]))
 p1 = FBSParams(
     u₁max=umax[1], u₂max=0.0, u₃max=0.0,
     w₁=weights1.w1, w₂=weights1.w2, w₃=weights1.w3, w₄=weights1.w4, w₅=weights1.w5,
-    max_iter=1000, relax=0.75, verbose=false
+    max_iter=1000, relax=0.5, verbose=false
 )
 p2 = FBSParams(
     u₁max=0.0, u₂max=umax[2], u₃max=0.0,
     w₁=weights2.w1, w₂=weights2.w2, w₃=weights2.w3, w₄=weights2.w4, w₅=weights2.w5,
-    max_iter=1000, relax=0.75, verbose=false
+    max_iter=1000, relax=0.5, verbose=false
 )
 p3 = FBSParams(
     u₁max=0.0, u₂max=0.0, u₃max=umax[3],
     w₁=weights3.w1, w₂=weights3.w2, w₃=weights3.w3, w₄=weights3.w4, w₅=weights3.w5,
-    max_iter=1000, relax=0.75, verbose=false
+    max_iter=1000, relax=0.5, verbose=false
 )
 
 r1 = forward_backward_sweep(sys, pvec, tspan, p1);
